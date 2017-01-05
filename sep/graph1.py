e9ddb61a62e4097bdbf3c01dd19914dead098947
Lam <qllam@cogradmac187.nexus.uwaterloo.ca>
2017-01-05 13:33:08 -0500

    Print placeholder integers instead of actual edges

--- sep/graph1.py
+++ sep/graph1.py
@@ -7,10 +7,10 @@ V = range(1,5+1)
 E = [(1,2),(1,3),(1,5),(2,3),(2,4),(3,4),(4,5)]
 
 
-print(pretty(inequalities.deltas(V, E)))
+print(pretty(inequalities.deltas(V, E, True)))
 
 Vstar = range(1,4+1)
 # there is a duplicate edge
 Estar = [(1,2),(1,3),(1,4),(2,3),(2,4),(3,4),(3,4)]
 
-print(pretty(inequalities.deltas(Vstar, Estar)))
+print(pretty(inequalities.deltas(Vstar, Estar, True)))
--- sep/inequalities.py
+++ sep/inequalities.py
@@ -8,12 +8,17 @@ edges: list of iterables, each set containing two integers
 
 """
 Returns a list of pairs of subsets S of V to  delta(S)
+
+usePlaceholders: Print 1, 2, 3, ..., |E| instead of the actual edge
 """
-def deltas(vertices, edges):
+def deltas(vertices, edges, usePlaceholders=False):
 	import itertools
 	for i in range(1, len(vertices)):
 		for S in itertools.combinations(vertices, i):
-			yield (S, [e for e in edges if (e[0] in S and e[1] not in S) or (e[0] not in S and e[1] in S)])
+			if not usePlaceholders:
+				yield (S, [e for e in edges if (e[0] in S and e[1] not in S) or (e[0] not in S and e[1] in S)])
+			elif usePlaceholders:
+				yield (S, [i for i,e in enumerate(edges, 1) if (e[0] in S and e[1] not in S) or (e[0] not in S and e[1] in S)])
 
 def inequalities(vertices, edges):
 	pass	
