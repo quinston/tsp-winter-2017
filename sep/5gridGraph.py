bc6ee917a98f8a2695c3bdf1ba4f27417ca9c197
Lam <qllam@cogradmac187.nexus.uwaterloo.ca>
2017-01-11 15:07:49 -0500

    Add grid graph maker scripts

--- sep/5gridGraph.py
+++ sep/5gridGraph.py
@@ -1,16 +1,10 @@
 import findTspCps
+import gridGraph
+import itertools
 
-V= range(1,16+1)
-
-E = [(a,a+1) for a in range(1, 4+1)] + [(a,a+5) for a in range(1,5+1)] +[(a,a+1) for a in range(6,9+1)] +[(a,a+5) for a in range(6,10+1)] +[(a,a+1) for a in range(11, 14+1)] +[(a,a+5) for a in range(11, 15+1)] +[(a,a+5) for a in range(11,15+1)] +[(a,a+1) for a in range(16,19+1)] +[(a,a+1) for a in range(21, 24+1)] 
-
-Vstar = [(a,a+1,a+5,a+6) for a in range(1,4+1)] +[(a,a+1,a+5,a+6) for a in range(6, 9+1)] +[(a,a+1,a+5,a+6) for a in range(11,14+1)] +[(a,a+1,a+5,a+6) for a in range(16, 19+1)] +[(1,2,3,4,5,10,15,20,25,24,23,22,21,16,11,6)]
-
-Estar = [(a, 17) for a in range(1,4+1)] + [(1,17)] + [(a, a-1) for a in range(2,4+1)] + [(4,17)] + [(a,a+4) for a in range(1,4+1)] + [(5,17)] + [(a, a-1) for a in range(6,8+1)] + [(8,17)] + [(a,a+4) for a in range(5,8+1)] + [(9,17)] + [(a, a-1) for a in range(10,12+1)] + [(12,17)] + [(a,a+4) for a in range(9,12+1)] + [(13,17)] + [(a, a-1) for a in range(14,16+1)] + [(16,17)] + [(a,17) for a in range(13,16+1)]
-
+V, E, Vstar, Estar = gridGraph.gridGraph(5)
 vinf = 1
 
-import itertools
 weights = dict(itertools.product(E, (1,)))
 
 findTspCps.findCps(V, E, Vstar, Estar, vinf, weights)
---
+++ sep/gridGraph.py
@@ -0,0 +1,32 @@
+
+import itertools
+
+"""
+Makes a grid graph of dimension k by k
+"""
+def gridGraph(k):
+	lastFace = (k-1)**2 + 1
+	return (
+		list(range(1, k*k+1)),
+		list(itertools.chain(*[itertools.chain(*[
+# left to right edges
+			[(a, a+1) for a in range(k*i + 1, k*i + (k-1) + 1)],
+# upper to lower edges
+			[(a, a+k) for a in range(k*i + 1, k*i + k + 1)]
+]) for i in range(k-1)])) + [(a, a+1) for a in range(k*(k-1)+1, k*(k-1) + (k-1) + 1)],
+# don't forgrt the infinite face with vertices in clockwise order
+		list(itertools.chain(*[[(a,a+1,a+k,a+k+1) for a in range(k*i + 1, k*i + (k-1) + 1)] for i in range(k-1)])) + [tuple(itertools.chain(range(1, k+1), range(k*2, k*(k-1)+1, k), range(k*k, (k-1)*k, -1), range((k-2)*k + 1, k, -k)))],
+		list(itertools.chain(
+# top to infinite face
+[(a, lastFace) for a in range(1, (k-1)+1)],
+# left to infinite face, left face to right face, right to infinite face
+[(1, lastFace)] + [(a, a+1) for a in range(1, (k-2)+1)] + [(k-1, lastFace)],
+# k-2 rows in between
+*[
+[(b-(k-1), b) for b in range(a, a + (k-1-1) + 1)] + [(a, lastFace)] + [(b, b+1) for b in range(a, a + (k-2-1) + 1)] + [(a + (k-2), lastFace)]
+for a in range((k-1)+1, (k-1)*(k-1), k-1)
+],
+# bottom to infinite face
+[(a, lastFace) for a in range((k-2) * (k-1) + 1, (k-1)*(k-1) + 1)]
+))
+)
