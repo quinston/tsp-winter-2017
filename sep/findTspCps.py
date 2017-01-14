160741c913f5cf0619ed399f28d092de866f4865
Q L <lklq@live.com>
2017-01-14 13:31:27 -0500

    Add restrictions to find cutting planes only in extra variables

--- sep/findTspCps.py
+++ sep/findTspCps.py
@@ -50,18 +50,58 @@ def findCps(vertices, edges, dualVertices, dualEdges, vinf, weights=None):
 		pointToSeparate = polytopeProb.solution.get_values()
 		print('Initial point: {}'.format(sparselyLabel(pointToSeparate)))
 
+		def getPositiveSupport(x):
+				return set((i+1) for i in range(len(x)) if x[i] > 0)
+
 		# This function takes care of filling in a_{XX} <- u^T A_{XX} where we had previously omitted a_{XX} variables  from the CG-cut system due to no positive support in pointToSeparate
 		def cpVectorFromProb(prob, pointToSeparate, A):
-			positiveSupport = set((i+1) for i in range(len(pointToSeparate)) if pointToSeparate[i] > 0)
 			u = [prob.solution.get_values('u{}'.format(i)) for i in range(1, len(A)+1)]
-			return [(prob.solution.get_values('a{}'.format(i)) if i in positiveSupport else math.floor(sum(u[j] * row[i-1] for j,row in enumerate(A)))) for i in range(1, len(variableNames) + 1)]
+			return [(prob.solution.get_values('a{}'.format(i)) if i in getPositiveSupport(pointToSeparate) else math.floor(sum(u[j] * row[i-1] for j,row in enumerate(A)))) for i in range(1, len(variableNames) + 1)]
 
 
 		noCuttingPlanes = 0
 		cpViolation = 1e20
-		while noCuttingPlanes == 0 or  cpViolation > 1e-5:
+		d = 0.01
+		while noCuttingPlanes == 0 or  cpViolation > d:
+			positiveSupport = getPositiveSupport(pointToSeparate)
+			cpProb = cgsep.makeCgLp(pointToSeparate, A, b, d)
 
-			cpProb = cgsep.makeCgLp(pointToSeparate, A, b, 0.01)
+			# Here we modify the default CGSEP programme for our educational purposes
+
+			# don't care about sparse combinations
+			cpProb.objective.set_linear([("u{}".format(i), 0) for i in range(1, len(A)+1)])
+
+			# force x_e = u^T A_j = 0 for cutting plane 
+			cpProb.linear_constraints.add(
+					lin_expr = [cplex.SparsePair(ind=["u{}".format(i) for i in range(1, len(A)+1)], 
+						val=[row[j-1] for row in A]) for j in range(1, len(edges)+1)],
+					rhs=[0] * len(edges), 
+					senses='E' * len(edges))
+
+			# force z_e,v >= 0
+			cpProb.linear_constraints.add(
+					lin_expr = [cplex.SparsePair(ind=["u{}".format(i) for i in range(1, len(A)+1)], 
+						val=[row[j-1] for row in A]) for j in range(len(edges)+1, len(variableNames)+1)],
+					rhs=[0] * (len(variableNames) - len(edges)), 
+					senses='L' * (len(variableNames) - len(edges)))
+
+
+			"""
+			# disincentivize original variables x_e, incentivize new variables z_e,v
+			# when a_k is negative, it gets floored to a nonzero negative number, which
+			# becomes positive when we flip the inequality
+			# when a_k is positive, it gets floored to 0
+			cpProb.variables.add(names=["v{}".format(i) for i in positiveSupport], 
+					obj=[(-1e-3 if i <= len(edges) else +1e-5) for i in positiveSupport],
+					types=[cpProb.variables.type.binary] * len(positiveSupport))
+			for i in positiveSupport:
+				cpProb.indicator_constraints.add(indvar="v{}".format(i),
+						lin_expr=cplex.SparsePair(ind=["a{}".format(i)], val=[1]),
+						rhs = -d,
+						sense="L")
+			"""
+                                
+
 			cpProb.write('cg.lp')
 	
 			cpProb.set_results_stream(None)
@@ -70,9 +110,12 @@ def findCps(vertices, edges, dualVertices, dualEdges, vinf, weights=None):
 	
 			cpVector = [x for x in cpVectorFromProb(cpProb, pointToSeparate, A)]
 			cpLabelledVector = sparselyLabel(cpVector)
+			if len(cpLabelledVector) == 0:
+				print("Done")
+				break
 			cpDistance = cpProb.solution.get_values()[0]
 
-			cpViolation = cpProb.solution.get_objective_value()
+			cpViolation = sum([c * x for c,x in zip(cpVector, pointToSeparate)]) - cpDistance 
 
 			print("Found cutting plane: ", cpLabelledVector)
 			# print a0
