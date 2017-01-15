2dac5160e47481d19aba7ce218ef7d3f68091885
Q L <lklq@live.com>
2017-01-14 22:14:00 -0500

    Got the up down face calculation correct

--- sep/gridDisplayer.py
+++ sep/gridDisplayer.py
@@ -11,44 +11,72 @@ class GridGraph(Canvas):
 		self.data = data
 
 		self.GRID_WIDTH = width
-		self.CELL_PIXEL_WIDTH = 20
-		self.ARROW_SIZE = 8
-		self.TOP_LEFT_CORNER = (5,5)
+		self.CELL_PIXEL_WIDTH = 30
+		self.ARROW_PIXEL_LENGTH = self.CELL_PIXEL_WIDTH // 2
+		self.TOP_LEFT_CORNER = (30, 30)
 
 		for i in range(1, (2*width - 1) * (width - 1) + (width-1) + 1):
 			self.drawEdge(i)
+			self.drawArcs(i)
 
 		
 	def drawEdge(self, e):
-		if "e{}".format(e) in self.data:
-			lineColour = "#{:02x}0000".format(int(255 * self.data["e{}".format(e)]))
-			if (e-1) % (self.GRID_WIDTH * 2 - 1) < (self.GRID_WIDTH - 1):
-				# Horizontal line
-				horizontalPosition = (e-1) % (self.GRID_WIDTH * 2 - 1)
-				verticalPosition = (e-1) // (self.GRID_WIDTH * 2 - 1)
-				self.create_line(self.TOP_LEFT_CORNER[0] + self.CELL_PIXEL_WIDTH * horizontalPosition,
-						self.TOP_LEFT_CORNER[1] + self.CELL_PIXEL_WIDTH * verticalPosition,
-						self.TOP_LEFT_CORNER[0] + self.CELL_PIXEL_WIDTH * (horizontalPosition + 1),
-						self.TOP_LEFT_CORNER[1] + self.CELL_PIXEL_WIDTH * verticalPosition,
-						fill=lineColour,
-						width='2')
-			else:
-				horizontalPosition = ((e-1) % (self.GRID_WIDTH * 2 - 1)) - (self.GRID_WIDTH - 1)
-				verticalPosition = ((e-1) // (self.GRID_WIDTH * 2 - 1)) 
-				# Vertical line
-				self.create_line(self.TOP_LEFT_CORNER[0] + self.CELL_PIXEL_WIDTH * horizontalPosition,
-						self.TOP_LEFT_CORNER[1] + self.CELL_PIXEL_WIDTH * verticalPosition,
-						self.TOP_LEFT_CORNER[0] + self.CELL_PIXEL_WIDTH * horizontalPosition,
-						self.TOP_LEFT_CORNER[1] + self.CELL_PIXEL_WIDTH * (verticalPosition + 1),
-						fill=lineColour,
-						width='2')
-
-
-
-		pass
+		lineColour = "#{:02x}0000".format(int(255 * self.data["e{}".format(e)]))
+		if (e-1) % (self.GRID_WIDTH * 2 - 1) < (self.GRID_WIDTH - 1):
+			# Horizontal line
+			horizontalPosition = (e-1) % (self.GRID_WIDTH * 2 - 1)
+			verticalPosition = (e-1) // (self.GRID_WIDTH * 2 - 1)
+			self.create_line(self.TOP_LEFT_CORNER[0] + self.CELL_PIXEL_WIDTH * horizontalPosition,
+					self.TOP_LEFT_CORNER[1] + self.CELL_PIXEL_WIDTH * verticalPosition,
+					self.TOP_LEFT_CORNER[0] + self.CELL_PIXEL_WIDTH * (horizontalPosition + 1),
+					self.TOP_LEFT_CORNER[1] + self.CELL_PIXEL_WIDTH * verticalPosition,
+					fill=lineColour,
+					width='2')
+		else:
+			horizontalPosition = ((e-1) % (self.GRID_WIDTH * 2 - 1)) - (self.GRID_WIDTH - 1)
+			verticalPosition = ((e-1) // (self.GRID_WIDTH * 2 - 1)) 
+			# Vertical line
+			self.create_line(self.TOP_LEFT_CORNER[0] + self.CELL_PIXEL_WIDTH * horizontalPosition,
+					self.TOP_LEFT_CORNER[1] + self.CELL_PIXEL_WIDTH * verticalPosition,
+					self.TOP_LEFT_CORNER[0] + self.CELL_PIXEL_WIDTH * horizontalPosition,
+					self.TOP_LEFT_CORNER[1] + self.CELL_PIXEL_WIDTH * (verticalPosition + 1),
+					fill=lineColour,
+					width='2')
 
 	def drawArcs(self, e):
-		pass
+		isVertical = (e-1) % (self.GRID_WIDTH * 2 - 1) < (self.GRID_WIDTH - 1)
+		isExterior = (((e-1) % (self.GRID_WIDTH * 2 - 1)) in [self.GRID_WIDTH - 1, self.GRID_WIDTH * 2 - 2]) or (((e-1) // (self.GRID_WIDTH * 2 - 1)) in [0, self.GRID_WIDTH])
+		unboundedFace = (self.GRID_WIDTH - 1)**2 + 1
+
+		if isVertical:
+			horizontalPosition = (e-1) % (self.GRID_WIDTH * 2 - 1)
+			verticalPosition = (e-1) // (self.GRID_WIDTH * 2 - 1)
+
+			upFace, downFace = (0, 0)
+			if verticalPosition == 0:
+				upFace = unboundedFace 
+				downFace = horizontalPosition + 1 
+			elif verticalPosition == self.GRID_WIDTH - 1:
+				upFace = (verticalPosition - 1) * (self.GRID_WIDTH - 1) + horizontalPosition + 1
+				downFace = unboundedFace
+			else:
+				upFace = (verticalPosition - 1) * (self.GRID_WIDTH - 1) + horizontalPosition + 1
+				downFace = (verticalPosition - 0) * (self.GRID_WIDTH - 1) + horizontalPosition + 1
+			print("direct {} up {} down {}".format(e, upFace, downFace))
+
+
+			# down arrow 
+			self.create_line(self.TOP_LEFT_CORNER[0] + (self.CELL_PIXEL_WIDTH//3) + self.CELL_PIXEL_WIDTH * horizontalPosition,
+					self.TOP_LEFT_CORNER[1] - (self.ARROW_PIXEL_LENGTH // 2) + self.CELL_PIXEL_WIDTH * verticalPosition,
+					self.TOP_LEFT_CORNER[0] + (self.CELL_PIXEL_WIDTH//3) + self.CELL_PIXEL_WIDTH * horizontalPosition,
+					self.TOP_LEFT_CORNER[1] + (self.ARROW_PIXEL_LENGTH // 2) + self.CELL_PIXEL_WIDTH * verticalPosition,
+					arrow = "last")
+			# up arrow
+			self.create_line(self.TOP_LEFT_CORNER[0] + (2 * self.CELL_PIXEL_WIDTH//3) + self.CELL_PIXEL_WIDTH * horizontalPosition,
+					self.TOP_LEFT_CORNER[1] - (self.ARROW_PIXEL_LENGTH // 2) + self.CELL_PIXEL_WIDTH * verticalPosition,
+					self.TOP_LEFT_CORNER[0] + (2 * self.CELL_PIXEL_WIDTH//3) + self.CELL_PIXEL_WIDTH * horizontalPosition,
+					self.TOP_LEFT_CORNER[1] + (self.ARROW_PIXEL_LENGTH//2) + self.CELL_PIXEL_WIDTH * verticalPosition,
+					arrow = "first")
 
 
 g1 = GridGraph(root, width=6, data=dict((("e{}".format(i), 1) for i in range(1, 60+1))))
