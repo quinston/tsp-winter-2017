e76c5c451f91c5f9f5b70835e2f51e850e0bb91d
Lam <qllam@cogradmac187.nexus.uwaterloo.ca>
2017-01-17 10:58:39 -0500

    Add rectangular grids

--- sep/gridGraph.py
+++ sep/gridGraph.py
@@ -32,11 +32,42 @@ for a in range((k-1)+1, (k-1)*(k-1), k-1)
 ))
 )
 
+"""
+k rows by l columns
+"""
+def rectangularGridGraph(k, l):
+	lastFace = (k-1)*(l-1) + 1
+	return (
+		list(range(1, k*l+1)),
+		list(itertools.chain(*[itertools.chain(*[
+# left to right edges
+			[(a, a+1) for a in range(l*i + 1, l*i + (l-1) + 1)],
+# upper to lower edges
+			[(a, a+l) for a in range(l*i + 1, l*i + l + 1)]
+]) for i in range(k-1)])) + [(a, a+1) for a in range(l*(k-1)+1, l*(k-1) + (l-1) + 1)],
+# don't forgrt the infinite face with vertices in clockwise order
+		list(itertools.chain(*[[(a,a+1,a+l,a+l+1) for a in range(l*i + 1, l*i + (l-1) + 1)] for i in range(k-1)])) + [tuple(itertools.chain(range(1, l+1), range(l*2, l*(k-1)+1, l), range(k*l, (k-1)*l, -1), range((k-2)*l + 1, l, -l)))],
+		list(itertools.chain(
+# top to infinite face
+[(a, lastFace) for a in range(1, (l-1)+1)],
+# left to infinite face, left face to right face, right to infinite face
+[(1, lastFace)] + [(a, a+1) for a in range(1, (l-2)+1)] + [(l-1, lastFace)],
+# k-2 rows in between
+*[
+[(b-(l-1), b) for b in range(a, a + (l-1-1) + 1)] + [(a, lastFace)] + [(b, b+1) for b in range(a, a + (l-2-1) + 1)] + [(a + (l-2), lastFace)]
+for a in range((l-1)+1, (k-1)*(l-1), l-1)
+],
+# bottom to infinite face
+[(a, lastFace) for a in range((k-2) * (l-1) + 1, (k-1)*(l-1) + 1)]
+))
+)
+
 if __name__ == '__main__':
 	gridWidth = int(sys.argv[1])
 	V, E, Vstar, Estar = gridGraph(gridWidth)
 	vinf = int(sys.argv[2])
 	weights = dict(itertools.product(E, (1,)))
+	weights[(3,4)] = weights[(3,8)] = weights[(8,9)] = weights[(4,9)] = 10
 
 	def outputGridDisplayerFormat(filename, pt):
 		with open(filename, 'w') as f:
