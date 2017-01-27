e04fd8ad98cfe419f56487005912e397eea5df78
Lam <qllam@cogradmac187.nexus.uwaterloo.ca>
2017-01-27 09:38:07 -0500

    Move things to Python3 because it's too much work to support python 2
    Use sparse matrix at the beginning

--- sep/mod2cutSolver.py
+++ sep/mod2cutSolver.py
@@ -4,13 +4,18 @@ import cplex
 from cplex.exceptions import CplexError
 import inequalities
 
+try:
+	xrange
+except NameError:
+	xrange = range
+
 """
 Ab: (A|b) the constraints and rhs put together in a dok_matrix. Modifies this in-place
 """
 def mod2rref(Ab):
 	# Reduce whole matrix mod 2
-	for key, value in Ab.iteritems():
-		Ab[key] = value % 2
+	for key in list(Ab.keys()):
+		Ab[key] = Ab[key] % 2
 
 	# Gauss Jordan elimination
 	noRows, noColumns = Ab.shape
@@ -27,7 +32,12 @@ def mod2rref(Ab):
 				rowIndicesToAddPivotRowTo = [i for i in xrange(noRows) if i != pivot and Ab[i, numColumn] == 1]
 
 				def symmetricDifference(row1, row2):
-					return set(row1.iteritems()) ^ set(row2.iteritems())
+					try:
+						return set(row1.keys()) ^ set(row2.keys())
+					except AttributeError as e:
+						print(dir(row1))
+						print(dir(row2))
+						raise e
 
 
 				for index in rowIndicesToAddPivotRowTo:
@@ -36,7 +46,7 @@ def mod2rref(Ab):
 					Ab[index, :] = 0
 
 					# Fill the row
-					for k,v in newRow:
+					for k in newRow:
 						Ab[index, k[1]] = 1
 
 				# Bring pivot row to top
@@ -109,7 +119,7 @@ def mod2cpBasis(Ab):
 		if i not in pivotVariables:
 			base = scipy.sparse.dok_matrix((noVariables, 1), 0, dtype='b')
 			base[i, 0] = 1
-			for numColumn, numRow in pivotVariablesToRowNumbers.iteritems():
+			for numColumn, numRow in pivotVariablesToRowNumbers.items():
 				base[numColumn, 0] = Ab[numRow, i]
 			basis.append(base)
 	
@@ -117,11 +127,12 @@ def mod2cpBasis(Ab):
 
 
 def mod2cutsLoop(vertices, edges, dualVertices, dualEdges, vinf, weights=None):
-	Ab = inequalities.makeExtendedLpConstraintMatrix(vertices, edges, dualVertices, dualEdges, vinf)
-	A = [row[0] for row in Ab]
-	b = [row[1] for row in Ab]
+	Ab = inequalities.makeSparseExtendedLpMatrix(vertices, edges, dualVertices, dualEdges, vinf, includeBounds=True)
+	A = Ab[:, :(Ab.shape[1] - 1)]
+	b = Ab[:, (Ab.shape[1] - 1)]
 
 	variableNames = inequalities.enumerateExtendedLpVariables(vertices, edges, dualVertices, dualEdges, vinf)
+	noVariables = len(variableNames)
 
 	def sparselyLabel(v):
 		return [(a,b) for a,b in zip(variableNames, v) if b != 0]
@@ -129,10 +140,19 @@ def mod2cutsLoop(vertices, edges, dualVertices, dualEdges, vinf, weights=None):
 	def rowToSparsePair(row):
 		return cplex.SparsePair(ind = [name for i,name in enumerate(variableNames) if row[i] != 0], val = [x for x in row if x != 0])
 
-	Alol = [rowToSparsePair(row) for row in A]
+	def dokRowToSparsePair(row):
+		return cplex.SparsePair(ind = [name for i,name in enumerate(variableNames) if row[0, i] != 0], 
+val = [int(row[i]) for i in sorted(row.keys())])
+
+	def matrixRowToSparsePair(row):
+		return cplex.SparsePair(ind = [name for i,name in enumerate(variableNames) if row[0, i] != 0],
+val = [row[0, i] for i in range(row.shape[1]) if row[0,i] != 0])
+
+	Alol = [dokRowToSparsePair(A[i, :]) for i in xrange(A.shape[0])]
+	bList = [int(b[i, 0]) for i in range(b.shape[0])]
 
 	if weights == None:
-		objectiveFunction = [1] * len(edges)
+		objectiveFunction = [1] * len(edges) + [0] * (len(variableNames) - len(edges))
 	else:
 		objectiveFunction = [weights[e] for e in edges] + [0] * (len(variableNames) - len(edges))
 
@@ -142,8 +162,10 @@ def mod2cutsLoop(vertices, edges, dualVertices, dualEdges, vinf, weights=None):
 		polytopeProb.variables.add(names = variableNames, 
 			obj = objectiveFunction,
 			lb = [0] * len(variableNames))
-		polytopeProb.linear_constraints.add(lin_expr = Alol, rhs = b, senses = 'L' * len(b))
+
+		polytopeProb.linear_constraints.add(lin_expr = Alol, rhs = bList, senses = 'L' * len(bList))
 		
+
 		polytopeProb.solve()
 		if not polytopeProb.solution.is_primal_feasible():
 			print('Original subtour polytope is empty')
@@ -151,8 +173,84 @@ def mod2cutsLoop(vertices, edges, dualVertices, dualEdges, vinf, weights=None):
 
 		pointToSeparate = polytopeProb.solution.get_values()
 		print('Initial point: {}'.format(sparselyLabel(pointToSeparate)))
-		
+
+		""" 
+		The equations that x* satisfies tightly are all the equations, and
+		all the inequalities where x*_e or z*_e,v is 0
+
+		We will call this the equational support of x*
+		"""
+
+		noOriginalConstraints = A.shape[0]
+		# This counts the number of equations in the extended formulation, times 2 for <= and >=
+		noAlwaysTightInequalities = noOriginalConstraints - noVariables
+
 		while True:
-			pass
+			# This will hold (A|b)^T
+			systemAb = scipy.sparse.dok_matrix((A.shape[1] + 1, A.shape[0]))
+
+			systemAb[:, :noAlwaysTightInequalities] = scipy.sparse.vstack(
+				[
+					A[:noAlwaysTightInequalities, :].transpose(), 
+					b[:noAlwaysTightInequalities, :].transpose()
+				], format='dok')
+
+			numConstraint = noAlwaysTightInequalities
+			numColumnInSystemAb = noAlwaysTightInequalities
+			while numConstraint < noOriginalConstraints:
+				# Do not use constraints where x or z is nonzero
+				if pointToSeparate[numConstraint - noAlwaysTightInequalities] == 0:
+					systemAb[:noVariables, numColumnInSystemAb] = A[numConstraint, :].transpose()
+					systemAb[noVariables, numColumnInSystemAb] = b[numConstraint, 0]
+					numColumnInSystemAb += 1
+				numConstraint += 1
+
+			# Throw away remaining unused columns
+			systemAb.resize((systemAb.shape[0], numColumnInSystemAb))
+
+			# Add rhs: 0s everywhere except a 1 for b
+			systemToSolve = scipy.sparse.hstack([
+				systemAb, 
+				scipy.sparse.coo_matrix(([1], ([systemAb.shape[0] - 1], [0])), shape=(systemAb.shape[0], 1))
+			], format='dok')
+
+			basis = mod2cpBasis(systemToSolve)
+
+			# No more mod-2 cuts
+			if basis == []:
+				break
+			else:
+				listOfLhs = []
+				rhs = []
+
+				# Add cut from affine offset
+				cutAndDistance = systemAb.dot(basis[0])
+				cut = cutAndDistance[:noVariables, 0].transpose()
+				# round down RHS
+				distance = cutAndDistance[noVariables, 0] - 1
+				
+				listOfLhs.append(matrixRowToSparsePair(cut))
+				rhs.append(distance)
+
+				# Add cut for each basis vector
+				for basisVector in basis[1]:
+					cutAndDistance = systemAb.dot(basis[0] + basisVector)
+					cut = cutAndDistance[:noVariables, 0].transpose()
+					distance = cutAndDistance[noVariables, 0] - 1
+
+					listOfLhs.append(matrixRowToSparsePair(cut))
+					rhs.append(distance)
+
+			polytopeProb.linear_constraints.add(lin_expr=listOfLhs, rhs=rhs, senses='L' * len(rhs))
+			polytopeProb.solve()
+
+			if not polytopeProb.solution.is_primal_feasible():
+				print('Subtour polytope is empty')
+
+			pointToSeparate = polytopeProb.solution.get_values()
+			print('New point is {}'.format(sparselyLabel(pointToSeparate)))
+
+
+			
 	except CplexError as e:
 		raise e
