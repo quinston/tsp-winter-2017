150f38ca9994685b373d8f021bfe52c8fb1b7256
Lam <qllam@cogradmac187.nexus.uwaterloo.ca>
2017-03-30 13:06:56 -0400

    Sparse solution output, timestamped logging

--- stab/comblp.py
+++ stab/comblp.py
@@ -1,5 +1,12 @@
 import cplex
 import argparse
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+ch = logging.StreamHandler()
+ch.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+logger.addHandler(ch)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--no-stable-sets', type=int, help='desired number of stable sets')
@@ -34,32 +41,35 @@ def getDominoes():
 	return d
 
 d = getDominoes()
-print(d.verticesToContainingDominoes)
 
 try:
 	with cplex.Cplex() as cpx:
 		noDominoes = len(d.dominoToWeight)
 		allDominoVariableNames = ["x{}".format(i) for i in range(noDominoes)]
 
-		print("Adding {} binary variables".format(noDominoes))
+		logging.info("Adding {} binary variables".format(noDominoes))
 		cpx.variables.add(names = allDominoVariableNames, types=cpx.variables.type.binary * noDominoes)
 
-		print("Setting objective")
+		logging.info("Setting objective")
 		cpx.objective.set_sense(cpx.objective.sense.minimize)
 		for domino,weight in enumerate(d.dominoToWeight):
 			cpx.objective.set_linear("x{}".format(domino), weight)
 
-		print("Adding clique constraints")
+		logging.info("Adding clique constraints")
+		counter = 0
 		# The set of dominoes sharing any particular vertex form a clique
 		for v,dominoes in d.verticesToContainingDominoes.items():
 			# If only one tooth contains a given vertex, the constraint is redundant since the tooth variable is 0-1
 			if len(dominoes) > 1:
 				cpx.linear_constraints.add(lin_expr = [cplex.SparsePair(ind=["x{}".format(i) for i in dominoes], val=[1]*len(dominoes))], rhs=[1], senses='L')
+			counter += 1
+			if counter % 100 == 0:
+				logging.info("Added {} constraints".format(counter))
 		
-		print("Adding parity variable 'k'")
+		logging.info("Adding parity variable 'k'")
 		cpx.variables.add(names = ["k"], types=cpx.variables.type.integer, lb=[0])
 
-		print("Adding parity constraint sum(x) = 2k+3")
+		logging.info("Adding parity constraint sum(x) = 2k+3")
 		cpx.linear_constraints.add(lin_expr = [cplex.SparsePair(ind=allDominoVariableNames + ["k"], val=([1]*noDominoes) + [-2])], rhs=[3], senses='E')
 
 		cpx.parameters.mip.limits.populate.set(args.no_stable_sets)
@@ -68,13 +78,13 @@ try:
 
 		cpx.write('oddstab.lp')
 
-		print("Populating!")
+		logging.info("Populating!")
 		cpx.populate_solution_pool()
 
-		print("Printing solutions:")
+		logging.info("Printing solutions:")
 		for i in range(cpx.solution.pool.get_num()):
-			print("Solution {}: {}".format(i, cpx.solution.pool.get_values(i)))
-			print("Objective value {}: {}".format(i, cpx.solution.pool.get_objective_value(i)))
+			logging.info("Solution {}: {}".format(i, [j for j,x in enumerate(cpx.solution.pool.get_values(i)) if x == 1]))
+			logging.info("Objective value {}: {}".format(i, cpx.solution.pool.get_objective_value(i)))
 
 		
 except cplex.exceptions.CplexError as e:
